e0352a48e741c6642e1625a145f103b887516163
Filippo <filippo.galli.cr@gmail.com>
2024-03-21 16:07:31 +0100

    Minor fixes + boxplot

--- progetto.R
+++ progetto.R
@@ -1,11 +1,24 @@
 ############################ Library #############################
 
+library(parallel)
+library(iterators)
+library(foreach)
+library(doParallel)
+
 library(XML)
 library(dplyr)
 library("methods")
 library(dplyr)
 
+library(ggplot2)
+
 ######################### XML to Dataframe ########################
+
+print("Reading XML files...")
+
+# Set the number of cores to use
+numCores <- detectCores() - 1 # Leave one core free
+registerDoParallel(cores=numCores)
 
 # List all XML files in the directory
 file_list <- list.files(path = "data", pattern = "\\.xml$", full.names = TRUE)
@@ -13,30 +26,36 @@ file_list <- list.files(path = "data", pattern = "\\.xml$", full.names = TRUE)
 # Initialize an empty list to store dataframes
 all_dataframes <- list()
 
-# Create a progress bar
-pb <- txtProgressBar(min = 0, max = length(file_list), style = 3)
-
-# Loop through each file, read it, and convert to a dataframe
-for (i in 1:length(file_list)) {
+# Parallel loop to read XML files and convert to dataframes
+all_dataframes <- foreach(i = 1:length(file_list), .packages = "XML") %dopar% {
   result <- xmlToDataFrame(file_list[i])
-  all_dataframes[[file_list[i]]] <- result
-  # Update the progress bar
-  setTxtProgressBar(pb, i)
+  return(list(name = file_list[i], data = result))
 }
 
-# Close the progress bar
-close(pb)
+# Manually combine the results into a named list
+named_dataframes <- list()
+for (res in all_dataframes) {
+  named_dataframes[[res$name]] <- res$data
+}
 
-# Combine all dataframes into one
-df <- bind_rows(all_dataframes)
+# Stop the parallel backend
+stopImplicitCluster()
+
+# Extract the 'data' component from each element
+dataframes_list <- lapply(all_dataframes, function(x) x$data)
+
+# Combine all dataframes into one using bind_rows
+df <- bind_rows(dataframes_list)
 
 # Print or access the results as needed
 head(df)
 
 # useless variables cleaning
-rm(all_dataframes, file_list, i, result)
+rm(all_dataframes, file_list, dataframes_list, named_dataframes, res, numCores)
 
 ######################### Data Cleaning #############################
+
+print("Data Cleaning...")
 
 # Clean all the rows without all zones
 table(df$ZonaMercato)
@@ -50,13 +69,12 @@ correct_tipo <- c('OFF')
 df <- df[!df$Tipo != correct_tipo, ]
 table(df$Tipo)
 
-# Clean all NA's rows (garbage of xml -> dataframe transformation)
-df <- na.omit(df) 
-
 # dropping column with only one value
 df <- subset(df, select = -c(Tipo, ZonaMercato, Mercato, element))
 
 ######################### Data Pre-processing #############################
+
+print("Data Prepocessing...")
 
 # Convert the 'Date' column from character to date
 df <- df %>%
@@ -70,9 +88,9 @@ df <- df %>%
 df <- df %>%
  mutate(Quantita = as.numeric(Quantita))
 
-# Convert the 'Ora' column from character to numeric 
+# Convert the 'Ora' column from character to factor 
 df <- df %>%
- mutate(Ora = as.numeric(Ora))
+ mutate(Ora = as.factor(Ora))
 
 # Convert the 'PrezzoZonale' column from character to numeric
 
@@ -80,3 +98,21 @@ df <- df %>%
  mutate(PrezzoZonale = as.numeric(PrezzoZonale))
 
 summary(df)
+
+# Clean all NA's rows (garbage of xml -> dataframe transformation)
+df <- na.omit(df) 
+
+######################### Data Processing #############################
+
+########################## Hour's Boxplot #############################
+
+# creating a boxplot
+df$Ora <- factor(df$Ora, levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"))
+
+ggplot(df,aes(Ora,Prezzo, fill = Ora)) + 
+geom_boxplot() + 
+ggtitle("Boxplot of Prezzo by Ora")  + 
+labs(x = "Ora", y = "Prezzo") +
+ylim(0, 250) +
+scale_fill_manual(values = rainbow(length(unique(df$Ora))))  # Assign a color palette to the levels of the factor
+
