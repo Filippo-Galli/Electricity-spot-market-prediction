3a44ac26e278d184c9133af0dfdf6113a1fde960
ClaudiooBarbieri <claudio.barbieri@mail.polimi.it>
2024-04-11 16:00:03 +0200

    non normalized curves

--- progetto.R
+++ progetto.R
@@ -239,7 +239,7 @@ ggplot(df_prezzoZonale, aes(x = Ora, y = PrezzoZonale, fill = Ora)) +
 # Cleaning variables
 rm(means_vector_hour, means_vector_month, sd_hour, sd_month, df_prezzoZonale, tot_mean, tot_sigma)
 
-########################## Curve of prezzo by hour #############################
+########################## Curve of prezzo by hour normalized #############################
 
 rm(list = ls())
 
@@ -355,3 +355,129 @@ ggplot(data, aes(x = unique_points, y = mean_values, color = Ora)) +
        y = "Mean Prezzo",
        title = "Mean Prezzo by Normalized Cumulative Quantity") +
   theme_minimal()
+
+
+
+
+########################## Curve of prezzo by hour #############################
+
+rm(list = ls())
+
+# Parameters
+desired_hours <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24)
+day <- c("2023-01-01", "2023-01-02", "2023-01-03", "2023-01-04", "2023-01-05", "2023-01-06", "2023-01-07", "2023-01-08", "2023-01-09", "2023-01-10", "2023-01-11", "2023-01-12", "2023-01-13", "2023-01-14", "2023-01-15", "2023-01-16", "2023-01-17", "2023-01-18", "2023-01-19", "2023-01-20", "2023-01-21", "2023-01-22", "2023-01-23", "2023-01-24", "2023-01-25", "2023-01-26", "2023-01-27", "2023-01-28", "2023-01-29", "2023-01-30", "2023-01-31")
+hours <- c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24")
+zona_mercato <- c("CALA;CNOR;CSUD;NORD;SARD;SICI;SUD;AUST;COAC;CORS;FRAN;GREC;SLOV;SVIZ;MALT;COUP;MONT;")
+
+# Load the data from .csv file
+df <- read.csv("csv/2023-01-01_to_2023-12-31.csv")
+
+# Dummy dataframe
+df_curve <- df[df$Data %in% day & df$Ora %in% hours & df$ZonaMercato %in% zona_mercato, ]
+# delete ZonaMercato
+df_curve <- df_curve %>% subset(select = c(Data, Ora, Prezzo, Quantita, PrezzoZonale))
+# mean prezzo zonale
+mean_prezzo_zonale <- mean(df_curve$PrezzoZonale)
+
+# remove variables useless
+rm(df, day, hours, zona_mercato)
+
+# calculate the cumulative sum of 'Quantita' for each 'Data' and 'Ora'
+df_curve <- df_curve %>%
+  group_by(Data, Ora) %>%
+  mutate(cum_sum_quantita = cumsum(Quantita))
+
+# Create dataframes from the splitting of df_curve by 'Ora' and 'Data'
+df_split_by_hour <- split(df_curve, interaction(df_curve$Ora, df_curve$Data))
+
+# delete empty dataframes
+df_split_by_hour <- df_split_by_hour[sapply(df_split_by_hour, nrow) > 0]
+
+# Apply the 'stepfun' function to each hour of each day to create a step function object
+step_fun_list <- lapply(df_split_by_hour, function(df) {
+  df <- df[order(df$cum_sum_quantita), ] # Sort by 'cum_sum_quantita_normalized'
+  y <- c(df$Prezzo, tail(df$Prezzo, n = 1)) # Add the last value to the end, otherwise error message
+  stepfun(df$cum_sum_quantita, y) # create the step function
+})
+
+# Function to find indexes of the desired hour in the list of dataframes
+find_hour_index <- function(desired_hour, list_of_dfs) {
+  # Convert desired_hour to a character for pattern matching
+  desired_hour <- as.character(desired_hour)
+  
+  # Initialize an empty vector to store the indices
+  indices <- c()
+  
+  # Loop through the names of the data frames
+  for (i in seq_along(list_of_dfs)) {
+    # Extract the hour from the name using a regular expression
+    # This pattern matches the first sequence of digits before the dot
+    hour <- regmatches(names(list_of_dfs)[i], regexpr("^\\d+", names(list_of_dfs)[i]))
+    
+    # Check if the extracted hour matches the desired hour
+    if (hour == desired_hour) {
+      # If a match is found, add the index to the indices vector
+      indices <- c(indices, i)
+    }
+  }
+  
+  # Return the indices vector
+  return(indices)
+}
+
+# Initialize an empty data frame to store the results of mean step functions
+data <- data.frame()
+
+for (desired_hour in desired_hours){
+  
+  # Find the index of the desired hour in the list of data frames
+  hour_index <- find_hour_index(desired_hour, df_split_by_hour)
+  
+  # Check if the hour_index is valid
+  if (length(hour_index) == 0) {
+    print("Desired hour not found in the list of data frames, hour: ", desired_hour)
+  }
+  
+  # Extract the step functions for the desired hour
+  # Assuming the step functions are in the same order as the data frames in df_split_by_hour
+  step_funs_desired_hour <- list()
+  for (i in seq_along(hour_index)) {
+    step_funs_desired_hour <- append(step_funs_desired_hour, step_fun_list[[hour_index[i]]])
+  }
+  
+  # Step 2: Find the unique points at which to evaluate the step functions
+  unique_points <- unique(unlist(lapply(df_split_by_hour, function(df) df$cum_sum_quantita)))
+  unique_points <- sort(unique_points)
+  
+  # Step 3: Evaluate the step functions at these points
+  values_desired_hour <- sapply(step_funs_desired_hour, function(step_fun) step_fun(unique_points))
+  
+  # Step 4: Calculate the mean of these values
+  mean_values <- rowMeans(values_desired_hour)
+  
+  # Step 5: Save mean values in a dummy dataframe
+  temp <- data.frame(unique_points, mean_values)
+  temp$Ora <- desired_hour
+  
+  # Step 6: Append the dummy dataframe to the main dataframe
+  data <- rbind(data, temp)
+}
+
+# Cleaning variables
+rm(temp, unique_points, mean_values, i, desired_hour, values_desired_hour, step_funs_desired_hour, hour_index)
+
+# Convert the 'Ora' column to a factor with the desired order
+data$Ora <- factor(data$Ora, levels = desired_hours)
+
+# Plot using ggplot2
+ggplot(data, aes(x = unique_points, y = mean_values, color = Ora)) +
+  geom_line() +
+  labs(x = "Quantity",
+       y = "Mean Prezzo",
+       title = "Mean OFF curves") +
+  theme_minimal() +
+  xlim(75000,105000) +
+  ylim(0,400) + 
+  geom_abline(slope = 0 , intercept = mean_prezzo_zonale, lty=2) +
+  geom_text(aes(x = 77000, y = mean_prezzo_zonale + 10, label = "mean PrezzoZonale"), 
+            color = "black", size = 3)
