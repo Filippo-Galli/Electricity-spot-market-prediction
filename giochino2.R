94517b148810eee1a85d2aad6277443a903d28e9
Filippo <filippo.galli.cr@gmail.com>
2024-07-10 16:14:13 +0200

    Giochino finished

--- giochino2.R
+++ giochino2.R
@@ -8,82 +8,103 @@ library(stats)
 library(readxl)
 
 od <- get_business_onedrive() #only one time
-#od$list_files()
-#od$open_item("Insert your offer or bid!.xlsx")
 
-#to be insert in a loop
-newfile <- od$get_item("Insert your offer or bid!.xlsx")
-newfile$download("responses.xlsx", overwrite = T)
-data <- read_excel(path="C:/Users/User/Documents/GitHub/Applied/responses.xlsx")
+# REMEMBER: set correctly the working directory and the file name
 
-
-data_off <- data[which(data$`Do you want to buy or sell electricity?`=="offer"), c(12,15)]
-data_bid <- data[which(data$`Do you want to buy or sell electricity?`=="bid"), c(18,21)]
-names(data_off) <- c("Quantita", "Prezzo")
-names(data_bid) <- c("Quantita", "Prezzo")
-data_off$Quantita <- data_off$Quantita*1000
-data_bid$Quantita <- data_bid$Quantita*1000
-# Group by day and hour, then sort each group by prezzo
-sorted.df.off <- data_off %>% arrange(Prezzo)
-sorted.df.bid <- data_bid %>% arrange(desc(Prezzo))
-
-# calculate cumulative sums
-cumulative.df.off <- sorted.df.off %>%
-  mutate(Quantita.sum = cumsum(Quantita))
-cumulative.df.bid <- sorted.df.bid %>%
-  mutate(Quantita.sum = cumsum(Quantita))
-quantity.range <- c(0, 5000000)  
-prezzo.range <- c(0, 600)
-
-restricted.plot.off <- list()
-restricted.plot.bid <- list()
-
-restricted.plot.off[[1]] <- cumulative.df.off %>%
-    filter(Quantita.sum >= quantity.range[1] & Quantita.sum <= quantity.range[2] & 
-              Prezzo >= prezzo.range[1] & Prezzo <= prezzo.range[2])
+find_step_intersections <- function(df1, df2){
+  fd.1 <- stepfun(df1$Quantita.sum[-1], df1$Prezzo)
+  fd.2 <- stepfun(df2$Quantita.sum[-1], df2$Prezzo)
   
-restricted.plot.bid[[1]] <- cumulative.df.bid %>%
-    filter( Quantita.sum >= quantity.range[1] & Quantita.sum <= quantity.range[2] & 
-              Prezzo >= prezzo.range[1] & Prezzo <= prezzo.range[2])
-
-plot(restricted.plot.bid[[1]]$Quantita.sum, restricted.plot.bid[[1]]$Prezzo, type='s',
-     xlim=c(0,max(restricted.plot.bid[[1]]$Quantita.sum)+1000), ylim=c(0,max(restricted.plot.bid[[1]]$Prezzo)+100), col='blue',
-     main = "Cumulative Sum of Quantity vs. Prezzo", xlab = "Cumulative Quantity", ylab = "Prezzo")
-points(restricted.plot.off[[1]]$Quantita.sum, restricted.plot.off[[1]]$Prezzo, type='s', col='red')
-
-#### intersection ----
-find_intersections <- function(df1, df2) {
-  intersections <- data.frame()
-  for (i in 1:(nrow(df1) - 1)) {
-    for (j in 1:(nrow(df2) - 1)) {
-      # Check if lines (x1, y1)-(x2, y2) and (x3, y3)-(x4, y4) intersect
-      a1 <- df1$Prezzo[i+1] - df1$Prezzo[i]
-      b1 <- df1$Quantita.sum[i] - df1$Quantita.sum[i+1]
-      c1 <- a1 * df1$Quantita.sum[i] + b1 * df1$Prezzo[i]
-      
-      a2 <- df2$Prezzo[j+1] - df2$Prezzo[j]
-      b2 <- df2$Quantita.sum[j] - df2$Quantita.sum[j+1]
-      c2 <- a2 * df2$Quantita.sum[j] + b2 * df2$Prezzo[j]
-      
-      det <- a1 * b2 - a2 * b1
-      if (det != 0) {
-        x <- (b2 * c1 - b1 * c2) / det
-        y <- (a1 * c2 - a2 * c1) / det
-        # Check if intersection point (x, y) is within line segments
-        if (x >= min(df1$Quantita.sum[i], df1$Quantita.sum[i+1]) && 
-            x <= max(df1$Quantita.sum[i], df1$Quantita.sum[i+1]) && 
-            x >= min(df2$Quantita.sum[j], df2$Quantita.sum[j+1]) && 
-            x <= max(df2$Quantita.sum[j], df2$Quantita.sum[j+1])) {
-          intersections <- rbind(intersections, data.frame(x = x, y = y))
-        }
-      }
-    }
-  }
-  return(intersections)
+  ## Curve subtraction
+  x.min <- max(min(df1$Quantita.sum), min(df2$Quantita.sum))
+  x.max <- min(max(df1$Quantita.sum), max(df2$Quantita.sum))
+  x_values <- seq(x.min, x.max, length.out = 10000)
+  
+  # Evaluate fd.off at x_values
+  y_off <- fd.1(x_values)
+  
+  # Evaluate fd.bid at x_values
+  y_bid <- fd.2(x_values)
+  
+  # Compute the difference between the two curves
+  difference_curve <- abs(y_off - y_bid)
+  
+  x.minimizer <- x_values[which.min(difference_curve)]
+  
+  intersection <- c(df2$Quantita.sum[which(df2$Quantita.sum>=x.minimizer)][1], fd.1(x.minimizer))
+  return (intersection)
 }
 
-mcp <- find_intersections(restricted.plot.off[[1]],restricted.plot.bid[[1]])
-mcp
+
+repeat {
+  
+  print("Catching new data...")
+  
+  # One drive connection
+  #to be insert in a loop
+  # newfile <- od$get_item("Insert your offer or bid!.xlsx")
+  # newfile$download("responses.xlsx", overwrite = T)
+  # data <- read_excel(path="C:/Users/User/Documents/GitHub/Applied/responses.xlsx")
+  
+  # local connection
+  data <- read_excel("Insert your offer or bid!.xlsx")
+  
+  names(data)[8] <- "Type"
+  names(data)[11] <- "OFF quant"
+  names(data)[14] <- "OFF price"
+  names(data)[17] <- "BID quant"
+  names(data)[20] <- "BID price"
+  
+  data <- data[, c(1, 2, 3, 4, 5, 8, 11, 14, 17, 20)]
+  
+  data_off <- data[which(data$Type=="offer"), c(7,8)]
+  data_bid <- data[which(data$Type=="bid"), c(9,10)]
+  names(data_off) <- c("Quantita", "Prezzo")
+  names(data_bid) <- c("Quantita", "Prezzo")
+  data_off$Quantita <- as.numeric(data_off$Quantita)
+  data_bid$Quantita <- as.numeric(data_bid$Quantita)
+  data_off$Prezzo <- as.numeric(data_off$Prezzo)
+  data_bid$Prezzo <- as.numeric(data_bid$Prezzo)
+  
+  
+  # Group by day and hour, then sort each group by prezzo
+  sorted.df.off <- data_off %>% arrange(Prezzo)
+  sorted.df.bid <- data_bid %>% arrange(desc(Prezzo))
+  
+  # calculate cumulative sums
+  cumulative.df.off <- sorted.df.off %>%
+    mutate(Quantita.sum = cumsum(Quantita))
+  cumulative.df.bid <- sorted.df.bid %>%
+    mutate(Quantita.sum = cumsum(Quantita))
+  quantity.range <- c(0, 5000000)  
+  prezzo.range <- c(0, 600)
+  
+  restricted.plot.off <- list()
+  restricted.plot.bid <- list()
+  
+  restricted.plot.off[[1]] <- cumulative.df.off %>%
+    filter(Quantita.sum >= quantity.range[1] & Quantita.sum <= quantity.range[2] & 
+             Prezzo >= prezzo.range[1] & Prezzo <= prezzo.range[2])
+  
+  restricted.plot.bid[[1]] <- cumulative.df.bid %>%
+    filter( Quantita.sum >= quantity.range[1] & Quantita.sum <= quantity.range[2] & 
+              Prezzo >= prezzo.range[1] & Prezzo <= prezzo.range[2])
+  
+  plot(restricted.plot.bid[[1]]$Quantita.sum, restricted.plot.bid[[1]]$Prezzo, ylim=c(0,max(restricted.plot.bid[[1]]$Prezzo)+100), col='blue',
+       main = "Cumulative Sum of Quantity vs. Prezzo", xlab = "Cumulative Quantity", ylab = "Prezzo", type = "s")
+  points(restricted.plot.off[[1]]$Quantita.sum, restricted.plot.off[[1]]$Prezzo, col='red', type = "s")
+  legend("topright", legend=c("Bid", "Offer"), col=c("blue", "red"), pch=15)
+  
+  #### intersection ----
+  mcp <- find_step_intersections(restricted.plot.off[[1]],restricted.plot.bid[[1]])
+  print(paste("Equilibrium point: x = ", mcp[1], ", y = ", mcp[2]))
+  abline(h=mcp[2], col="grey", lty=2)
+  abline(v=mcp[1], col="grey", lty=2)
+  
+  Sys.sleep(20)
+  
+}
+
 
 #### mail ----
 mail <- data$`Posta elettronica`
